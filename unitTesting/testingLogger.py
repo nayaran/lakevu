a683951705231b7840b6299bee8e6037346519d8
narayan <anuragallahabad@gmail.com>
2015-08-12 22:23:22 +0530

    adds comments in testingLogger file

--- unitTesting/testingLogger.py
+++ unitTesting/testingLogger.py
@@ -4,7 +4,7 @@ def testLogger():
       """
       A basic method for testing the logger
       """
-
+      # import the module to be tested
       # add the current parent directory to the python path
       from os import sys, path
       sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
@@ -15,6 +15,8 @@ def testLogger():
       # get child logger which would inherit the configuration
       # from the parent (LakvuLogger) logger
       logger = logging.getLogger('LakvuLogger.test')
+
+      # test the module
       logger.info('testing lakevu logger!!!!')
 
 
