547c4e7a1c0feb1c41a6c07efb518872c4ddff3c
narayan <anuragallahabad@gmail.com>
2015-08-13 01:46:26 +0530

    adds addNewUser and getAllUsers methods

--- dao/ExpenseMgmtDao.py
+++ dao/ExpenseMgmtDao.py
@@ -10,7 +10,6 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 # now the module is ready to be imported
 import log
-
 import logging
 
 # get the logger instance
@@ -23,20 +22,63 @@ def getConnection():
       """
       #mainLogger.info(":: inside getConnection :: ")
       client = MongoClient()
-      db = client['user']
+      #db = client['user']
       #db = client['expenseManagement']
+      db = client['testingDB']
       #mainLogger.info(":: returning Connection :: ")
       return db
 
 def getAllCollections():
       """
-      Get all the connections from the DB
+      Get all the collections from the DB
       """
       #mainLogger.info(":: inside getAllCollections :: ")
       db = getConnection()
       result = db.collection_names()
       #mainLogger.info(":: exiting getAllCollections :: ")
-      mainLogger.info("returning - %d connections", len(result))
+      mainLogger.info("returning - %d collections", len(result))
       return result
+
+def addNewUser(userDoc, collection):
+      """
+      Adds a new user to the DB
+      """
+      db = getConnection()
+      mainLogger.info("inserting in collection- %s", collection)
+      mainLogger.info("inserting document- \n%s", pformat(userDoc))
+      result = db[collection].insert_one(userDoc)
+      return result.inserted_id
+
+def getAllUsers(collection):
+      """
+      Returns all the users in a given group
+      """
+      db = getConnection()
+      result = db[collection].find()
+      mainLogger.info("fetching useres from collection- %s", collection)
+      mainLogger.info("total count of users fetched- %d", result.count())
+
+      users = []
+
+      for user in result:
+            users.append(user)
+
+      return users
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
