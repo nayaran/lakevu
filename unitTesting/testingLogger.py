42cd611cf1534a17cbc1e1797eda1b7e83489daf
narayan <anuragallahabad@gmail.com>
2015-08-12 20:44:59 +0530

    restructures the testingLogger contents

--- unitTesting/testingLogger.py
+++ unitTesting/testingLogger.py
@@ -1,12 +1,22 @@
 import logging
 
-if __name__ == '__main__':
+def testLogger():
+      """
+      A basic method for testing the logger
+      """
 
       # add the current parent directory to the python path
-
       from os import sys, path
       sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+      # now the module is ready to be imported
       import log
 
+      # get child logger which would inherit the configuration
+      # from the parent (LakvuLogger) logger
       logger = logging.getLogger('LakvuLogger.test')
       logger.info('testing lakevu logger!!!!')
+
+
+if __name__ == '__main__':
+      testLogger()
