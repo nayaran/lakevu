131297ad721d7f85bd0fd15a1f5ab01c4b2d97cb
narayan <anuragallahabad@gmail.com>
2015-08-14 00:53:55 +0530

    implements getUsers api

--- main.py
+++ main.py
@@ -5,21 +5,55 @@ from bottle import route
 from bottle import run
 from bottle import debug
 from bottle import request
+from bottle import response
 from bottle import error
 from bottle import Bottle
 from pprint import pprint
 from pprint import pformat
 
-import dao
+import dao.ExpenseMgmtDao as dao
+import json
+import log
 
+import logging
+
+# get the logger instance
+mainLogger = logging.getLogger('LakvuLogger.main')
 app = Bottle()
 
-@app.route('lakevu/', method='GET')
+@app.route('/lakevu', method='GET')
 def showMainPage():
       """
       Returns the main page
       """
       return "Welcome to lakevu!"
+
+
+
+@app.route('/lakevu/addUser', method='POST')
+def addUser():
+      """
+      Adds a new user
+      """
+      pass
+
+@app.route('/lakevu/getUsers', method='GET')
+def getUsers():
+      """
+      Adds a new user
+      """
+      mainLogger.info('handling request for getUsers')
+      collection = request.GET.get('collection').strip()
+      mainLogger.info('received: collection - %s', collection)
+
+      result = dao.getAllUsers(collection)
+      mainLogger.info('fetching data from backend...')
+      mainLogger.info('type(result)- %s', type(result))
+      mainLogger.info('returning-\n%s', result)
+
+      response.content_type = 'application/json'
+      from bson.json_util import dumps
+      return dumps(result)
 
 
 if __name__ == "__main__":
